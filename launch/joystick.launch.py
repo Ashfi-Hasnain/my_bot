f4c3c4ffcc7a2a8b6433f05d787873269ea7d046
Ashfi-Hasnain <Ashfihasnain39@gmail.com>
2025-08-20 22:52:10 -0400

    Hopefully finished debugging

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -1,6 +1,8 @@
 # This file introduces controller startup [Source 21]
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 import os
 from ament_index_python.packages import get_package_share_directory
@@ -26,10 +28,13 @@ def generate_launch_description():
 
     # Provisions for mobile control (Source 22)
     #twist_stamper = Node(
-        #package='twist_stamper',
-        #executable='twist_stamper,
-        #parameters=[joy_params, {'use_sim_time': use_sim_time}],
-        #remappings=[('/cmd_vel_in', '/diff_cont/cmd_vel_unstamped'),('/cmd_vel_out', '/diff_cont/cmd_vel')],
+    #    package='twist_stamper',
+    #    executable='twist_stamper',
+    #    parameters=[joy_params, {'use_sim_time': use_sim_time}],
+    #    remappings=[
+    #        ('/cmd_vel_in', '/diff_cont/cmd_vel_unstamped'),
+    #        ('/cmd_vel_out', '/diff_cont/cmd_vel')
+    #    ],
     #)
 
     return LaunchDescription([
--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -4,90 +4,88 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import IncludeLaunchDescription, TimerAction, RegisterEventHandler
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
-from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessStart
-
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
 
-
     # 1. Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
     package_name='my_bot'
 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'false'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'false'}.items()
     )
 
-     # Include the joystick launch file (Source 22)
+    # Include the joystick launch file (Source 22)
     joystick = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','joystick.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'false'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','joystick.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'false'}.items()
     )
 
     # 2. Define the path to the Gazebo parameters file (Source 18)
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config', 'gazebo_params.yaml')
 
-        #Source 20:
-        robot_description = Command('ros2 param get --hide /robot_state_publisher robot_description')
+    #Source 20:
+    robot_description = Command([
+        'xacro ',
+        os.path.join(get_package_share_directory(package_name), 'urdf', 'my_bot.urdf.xacro')
+    ])
 
-        controller_params_file = os.path.join(get_package_share_directory(package_name),'config', 'my_controllers.yaml')
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config', 'my_controllers.yaml')
 
-        controller_manager = Node(
-            package="controller_manager",
-            executable="ros2_control_node",
-            parameters=[{'robot_description': robot_description},
-                        controller_params_file]
-        )
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description},
+                    controller_params_file]
+    )
 
-        delayed_controller_manager = TimerAction(period=3.0,actions=[controller_manager])
+    delayed_controller_manager = TimerAction(period=3.0,actions=[controller_manager])
 
     # 3. Differental drive controller spawner
-        #Source 17:
-        diff_drive_spawner = Node(
-            package="controller_manager",
-            executable="spawner.py",
-            arguments=["diff_cont"],
-        )
+    #Source 17:
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
 
-        #Source 20:
-        delayed_diff_drive_spawner = RegisterEventHandler(
-            event_handler=OnProcessStart(
-                target_action=controller_manager,
-                on_start=[diff_drive_spawner],
-            )
+    #Source 20:
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
         )
+    )
 
     # 4. Joint broad caster spawner
-        #Source 17:
-        joint_broad_spawner = Node(
-            package="controller_manager",
-            executable="spawner.py",
-            arguments=["joint_broad"],
-        )
+    #Source 17:
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
 
-        #Source 20:
-        delayed_joint_broad_spawner = RegisterEventHandler(
-            event_handler=OnProcessStart(
-                target_action=controller_manager,
-                on_start=[diff_drive_spawner],
-            )
+    #Source 20:
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
         )
+    )
 
     # 5. Launch them all!
     return LaunchDescription([
         rsp,
         joystick,
-        delayed_controller_manager
+        delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
     ])
